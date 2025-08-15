b0e217ae20f554f4372521c7704afc92fa1068b1
Krambrek <225311930+Krambrek@users.noreply.github.com>
2025-08-15 13:19:52 +0300

    Replace ThreadSafeList with Event and Lock

--- thre.py
+++ thre.py
@@ -1,38 +1,20 @@
-from threading import Lock, Thread
-from time import sleep
+from queue import Empty, Queue
+from threading import Event, Thread
 
 from serial.tools.list_ports_windows import comports
 from serial import Serial
 from serial.serialutil import SerialException
 
 
-class ThreadSafeList():
-    def __init__(self, data):
-        self._lock = Lock()
-        self._list = list(data)
-
-    def __len__(self):
-        with self._lock:
-            return len(self._list)
-
-    def append(self, item):
-        with self._lock:
-            self._list.append(item)
-
-    def pop(self):
-        with self._lock:
-            return self._list.pop()
-
-
-def read(name, data, exit_):
+def read(name, data_queue, exit_event):
     try:
         port = Serial(name, timeout=0.05)
         print(f'connected to {name}')
         buff = bytes()
-        while not exit_:
+        while not exit_event.wait(0):
             buff += port.read_until(size=1)
             if len(buff) == 10:
-                data.append(buff.decode('utf-8').rstrip('\r\n'))
+                data_queue.put(buff.decode('utf-8').rstrip('\r\n'), block=True)
                 buff = bytes()
         print(f'disconnected from {name}')
         port.close()
@@ -42,23 +24,24 @@ def read(name, data, exit_):
 
 if __name__ == '__main__':
     readers = []
-    data = ThreadSafeList([])
-    exit_ = ThreadSafeList([])
+    data_queue = Queue()
+    exit_event = Event()
 
     for port in comports():
         if port.name in ['COM3', 'COM5']:
             continue
-        reader = Thread(target=read, name=f'{port.name} Reader', args=(port.name, data, exit_))
+        reader = Thread(target=read, name=f'{port.name} Reader', args=(port.name, data_queue, exit_event))
         readers.append(reader)
         reader.start()
 
     try:
         while True:
-            if len(data):
-                print(f'received {data.pop()}')
-            sleep(0.05)
+            try:
+                print(f'received {data_queue.get(block=True, timeout=0.05)}')
+            except Empty:
+                pass
     except KeyboardInterrupt:
-        exit_.append('EXIT')
+        exit_event.set()
 
     print('waiting for threads...')
 
